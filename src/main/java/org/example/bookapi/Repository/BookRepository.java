eb627729f89f8596c31e067190099dad5d945ee7
alistanikzay <ali.kawsar.stanikzay>
2025-06-02 01:04:51 +0200

    fixing things - Jakarta EE

--- src/main/java/org/example/bookapi/Repository/BookRepository.java
+++ src/main/java/org/example/bookapi/Repository/BookRepository.java
@@ -2,9 +2,15 @@ package org.example.bookapi.Repository;
 
 import jakarta.data.repository.BasicRepository;
 import org.example.bookapi.entity.Book;
+import java.util.List;
 
 public interface BookRepository extends BasicRepository<Book, Long> {
 
     <S extends Book> S save(S entity);
 
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByAuthorContainingIgnoreCaseAndTitleContainingIgnoreCase(String author, String title);
 }
